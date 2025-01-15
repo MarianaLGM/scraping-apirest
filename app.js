b8e096c94b9251223ad1b2e64ee77c8579086dac
MarianaLGM <mariana_lobeto@hotmail.com>
2025-01-15 20:27:35 +0100

    Mariana

--- app.js
+++ app.js
@@ -6,12 +6,29 @@
     - PUT actualizar una noticia existente
     - DELETE eliminar una noticia*/
 
-
+const scraping= require("./scraping.js")
 const express=require("express");
 const app= express();
+const fs = require("fs");
 
 app.use(express.json());// Middleware para manejar datos JSON
 app.use(express.urlencoded({ extended: true }));// Middleware para manejar datos de formularios URL-encoded
+
+app.get("/scraping", scraping)
+
+app.get("/", (req, res)=>{//accedemos todas las noticias para que nos devuelva todo el json
+
+    let noticias="";
+        try {
+          const data = fs.readFileSync('noticias.json', 'utf-8');
+          noticias = JSON.parse(data);
+          console.log(noticias)
+        } catch (error) {
+          console.error('Error al leer el archivo noticias.json:', error.message);
+        }
+    res.send(noticias);
+})
+
 
 
 app.listen(3000,() =>{
--- scraping.js
+++ scraping.js
@@ -1,25 +1,27 @@
 const axios=require("axios");
 const cheerio=require ("cheerio");
-const express=require("express");
-const app= express();
 
 const fs = require("fs");
 const PORT=3000;
 
 const url ="https://elpais.com/ultimas-noticias/" 
 
-const scrapingApirest=()=>{
-    app.get("/scraping", (req,res)=>{
-    //res.send("FUNCIONA!!!!")
-        axios.get(url).then((response)=>{//llamamos a axios
-            if(response.status === 200){
-            const html=response.data //guardar respuesta
-            const $=cheerio.load(html)// vamos a llamar a cheerio $ vamos a guardar todo en la variable
-            //console.log(html)esto lo que hará es cargar todo el html
+// Guardar datos en el archivo JSON
+function guardarDatos(noticias) {
+    fs.writeFileSync('noticias.json', JSON.stringify(noticias, null, 2));
+  }
+
+const scraping=(req,res)=>{ 
+    axios.get(url).then((response)=>{//llamamos a axios
+        if(response.status === 200){
+        const html=response.data //guardar respuesta
+        const $=cheerio.load(html)// vamos a llamar a cheerio $ vamos a guardar todo en la variable
+        //console.log(html)esto lo que hará es cargar todo el html
         
-            let noticias = [];
+        let noticias = [];
   
             $("article.c.c-d.c--m").each((index, element)=>{
+                console.log(element);
                 const imagen = $(element).find("img").attr("src");
                 const titulo = $(element).find("header.c_h").text();
                 const descripcion = $(element).find("p.c_d").text();
@@ -35,19 +37,13 @@ const scrapingApirest=()=>{
             };
                 noticias.push(noticia);
                 
-        });    
-        //res.send(noticias)
-            fs.writeFileSync('noticias.json', JSON.stringify(noticias, null, 2)); //aqui guardamos lo que acabamos de scrapear     
+        });  
+        guardarDatos(noticias);   
+        res.send(noticias);
             }
 
     });
 
-    })
     }
   
-app.listen(3000,() =>{
-    console.log("Express está escuchando en el puerto 3000")
-
-})
-
-module.exports=(scrapingApirest)
+module.exports=(scraping)
